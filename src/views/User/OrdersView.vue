035464e4eae86f50de11ed4ca4abbcc89dc1ed24
satanoki <florq228@gmail.com>
2025-04-22 22:56:20 +0300

    project_6

--- src/views/User/OrdersView.vue
+++ src/views/User/OrdersView.vue
@@ -1,14 +1,54 @@
 <template>
-  <v-container>
-    <v-row justify="center">
-      <h1>Orders</h1>
-    </v-row></v-container>
+    <v-container>
+        <v-row justify="center">
+            <v-col cols="12" sm="8" lg="6">
+                <h1 class="text--secondary mb-3 mt-3">Orders</h1>
+                <v-list subheader two-line flat>
+                    <v-list-item v-for="order in orders" :key="order.id">
+                        <template v-slot:prepend>
+                            <v-list-item-action>
+                                <v-checkbox :input-value="order.done" color="primary"
+                                    @click="markDone(order)"></v-checkbox>
+                            </v-list-item-action>
+                        </template>
+                        <v-list-item-title>{{ order.name }}</v-list-item-title>
+                        <v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
+
+                        <template v-slot:append>
+                            <v-list-item-action>
+                                <v-btn class="primary" :to="'/ad/' + order.adId">Open</v-btn>
+                            </v-list-item-action>
+                        </template>
+                    </v-list-item>
+                </v-list>
+
+
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
+
 <script>
 export default {
-  data() {
-    return {
-    }
-  }
+    data() {
+        return {
+            orders: [
+                {
+                    id: "123",
+                    name: "Timur",
+                    phone: "+7(978)777-77-77",
+                    adId: "1",
+                    done: true
+                }
+            ]
+        }
+    },
+    methods: {
+        markDone(order) {
+            order.done = !order.done;
+            console.log(order.done);
+        },
+    },
+
 }
 </script>
