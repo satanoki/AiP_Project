78a4eb09357b1b198736e032d9526867f3920d35
satanoki <florq228@gmail.com>
2025-04-22 22:46:51 +0300

    project_5

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,92 @@
 <template>
   <v-container>
     <v-row justify="center">
-      <h1>Home</h1>
-    </v-row></v-container>
+      <v-col cols="12" xs="12">
+        <v-carousel>
+          <v-carousel-item v-for="ad in ads" :key="ad.id" :src="ad.src" cover>
+            <div class="ad-link">
+              <v-btn class="error" :to="'/ad/' + ad.id">
+                {{ ad.title }}
+              </v-btn>
+            </div>
+          </v-carousel-item>
+        </v-carousel>
+      </v-col>
+    </v-row>
+  </v-container>
+  <v-container grid-list-lg>
+    <v-row justify="center">
+      <v-col cols="3" sm="3" md="3" lg="3" xs="12" v-for="ad in ads" :key="ad.id">
+        
+        <v-card>
+          <v-img :src="ad.src" height="200px"></v-img>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb0">{{ ad.title }}</h3>
+              <div>{{ ad.desc }}</div>
+            </div>
+          </v-card-title>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn text :to="'/ad/' + ad.id">Open</v-btn>
+            <v-btn raised color="primary">Buy</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
 <script>
+
 export default {
   data() {
     return {
+      ads: [
+        {
+          title: "First",
+          desc: "First Desc",
+          promo: true,
+          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+          id: "1"
+        },
+        {
+          title: "Second",
+          desc: "Second Desc",
+          promo: true,
+          src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+          id: "2"
+        },
+        {
+          title: "Third",
+          desc: "Thitd Desc",
+          promo: true,
+          src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+          id: "3"
+        },
+        {
+          title: "Fouth",
+          desc: "Fouth Desc",
+          promo: true,
+          src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+          id: "4"
+        }
+      ]
     }
   }
 }
 </script>
+
+<style scoped>
+.ad-link {
+  position: absolute;
+  bottom: 50px;
+  left: 50%;
+  background: rgb(0, 0, 0, 0.5);
+  transform: translate(-50%, 0);
+  padding: 5px 15px;
+  border-top-right-radius: 5px;
+  border-top-left-radius: 5px;
+}
+</style>
 
