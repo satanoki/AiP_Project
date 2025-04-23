ce3c0b8cdafbb7965fee8248a16971107c90bb36
satanoki <florq228@gmail.com>
2025-04-24 01:28:34 +0300

    project_19

--- src/store/orders.js
+++ src/store/orders.js
@@ -1,17 +1,29 @@
+class Order {
+    constructor(name, phone, adId, userId, done = false, id = null) {
+        this.name = name
+        this.phone = phone
+        this.adId = adId
+        this.userId = userId
+        this.done = done
+        this.id = id
+    }
+}
+
 export default {
 
     state: {
         orders: []
     },
 
-    mutations: {},
+    mutations: {
+        createOrder(state, payload) {
+            state.orders.push(payload)
+        },
+    },
 
     actions: {
         async createOrder({ commit }, { name, phone, adId, userId }) {
-            console.log(name)
-            console.log(phone)
-            console.log(adId)
-            console.log(userId)
+            let payload = new Order(name, phone, adId, userId, false, Math.random())
             commit('clearError')
             //Заглушка запроса
             let isRequestOk = true
@@ -21,6 +33,7 @@ export default {
             if (isRequestOk) {
                 await promise.then(() => {
                     //Здесь вызовем commit для добавления заказа
+                    commit('createOrder', payload)
                 })
             } else {
                 await promise.then(() => {
@@ -31,6 +44,10 @@ export default {
         }
     },
 
-    getters: {}
-
+    getters: {
+        orders(state, getters) {
+            if (getters.user == null) return []
+            return state.orders.filter(order => order.userId == getters.user.id)
+        }
+    },
 }
